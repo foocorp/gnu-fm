40c128fa86fe29b1093da3c226eb280b047e79b6
jurgbohn <>
2009-04-04 20:46:51 +0000

    Added preliminary Artist-class and XML-class. In artist: getInfo uses artist-name or musicbrainz-id to fetch artist from database. Read artist.README for converting to plaintext XML-form.

---
+++ gobbler/api/xml.php
@@ -0,0 +1,19 @@
+<?php
+class XML {
+    public static function prettyXML($xml) {
+	$dom = new DOMDocument('1.0'); 
+	$dom->preserveWhitespace = false;
+	$dom->loadXML($xml->asXML());  
+	$dom->formatOutput = true;
+	return($dom->saveXML());
+    } 
+
+    public static function error($status, $errcode, $errtext) {
+	$xml = new SimpleXMLElement("<lfm></lfm>");
+	$xml->addAttribute("status", $status);
+	$error = $xml->addChild("error", $errtext);
+	$error->addAttribute("code", $errcode);
+	return(XML::prettyXML($xml));
+    }
+}
+?>
