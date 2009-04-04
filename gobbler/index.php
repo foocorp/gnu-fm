324498f1856cc293ea90064cbf745698df9f8c3e
elleo <>
2009-04-04 15:05:41 +0000

    Keep track of the gobbler version a configuration was created with, so we can instruct the user to reinstall (or run an upgrade script in the future) if there's been an incompatible configuration or database change.

--- gobbler/index.php
+++ gobbler/index.php
@@ -1,6 +1,10 @@
 <?php
-
 require_once('database.php');
+require_once('version.php');
+
+if (!isset($config_version) || $config_version != $version) {
+	die("Configuration file is out of date. Please delete it (and associated database) and <a href='install.php'>reinstall</a>."); //TODO: Upgrade script for release versions
+}
 
 require_once('submissions-handshake.php');
 ?>
