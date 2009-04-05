15f96f813f6bb31d788eacd8c199be42ced0f75c
jurgbohn <>
2009-04-05 11:11:29 +0000

    Added 'getTopTracks' to artist, added User-class with getInfo. Changed return value from XML::error.

--- gobbler/api/artist.php
+++ gobbler/api/artist.php
@@ -2,6 +2,8 @@
 require_once('../database.php');
 require_once('./xml.php');
 
+print(XML::prettyXML(Artist::getTopTracks("Judas Priest")));
+
 class Artist {
 
     public static function getInfo($api_key, $artist, $mbid, $lang) {
@@ -21,27 +23,53 @@ class Artist {
 	}
 
 	if (PEAR::isError($res) || !$res->numRows()) {	    
-	    echo XML::error("failed", "7", "Invalid resource specified");
-	    return;
+	    return(XML::error("failed", "7", "Invalid resource specified"));
 	}
 
 	$xml = new SimpleXMLElement("<lfm status=\"ok\"></lfm>");
-	$row = $res->fetchRow();
+	$row = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
 
 	$artist = $xml->addChild("artist", null);
-	$artist->addChild("name", $row[0]);
-	$artist->addChild("mbid", $row[1]);
-	$artist->addChild("url", $row[9]);
-	$artist->addChild("streamable", $row[2]);
+	$artist->addChild("name", $row['name']);
+	$artist->addChild("mbid", $row['mbid']);
+	$artist->addChild("url", $row['url']);
+	$artist->addChild("streamable", $row['streamable']);
 
 	$bio = $artist->addChild("bio", null);
-	$bio->addChild("published", $row[3]);
-	$bio->addChild("summary", $row[5]);
-	$bio->addChild("content", $row[4]);
+	$bio->addChild("published", $row['bio_published']);
+	$bio->addChild("summary", $row['bio_summary']);
+	$bio->addChild("content", $row['bio_content']);
 
+	$res->free();
 
 	return($xml);
     }
+    
+    public static function getTopTracks($artist) {
+	global $mdb2;
+
+	$res = $mdb2->query("SELECT Track.*, COUNT(*) AS freq, COUNT(DISTINCT Scrobbles.username) AS dist FROM Scrobbles,Track WHERE Scrobbles.track = Track.name AND Track.artist =" . $mdb2->quote($artist, 'text') . " GROUP BY Track.name ORDER BY freq DESC");
+
+	if (PEAR::isError($res) || !$res->numRows()) {
+	    return(XML::error("failed", "7", "Invalid resource specified"));
+	}
+
+	$xml = new SimpleXMLElement("<lfm status=\"ok\"></lfm>");
+	$root = $xml->addChild("toptracks", null);
+	$root->addAttribute("artist", $artist);
+	$i = 1;
+	while (($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC))) {
+	    $track = $root->addChild("track", null);
+	    $track->addAttribute("rank", $i);
+	    $track->addChild("name", $row['name']);
+	    $track->addChild("mbid", $row['mbid']);
+	    $track->addChild("playcount", $row['freq']);
+	    $track->addChild("listeners", $row['dist']);
+	    $i++;
+	}
+	return($xml);	
+    }
+
 }
 
 ?>
