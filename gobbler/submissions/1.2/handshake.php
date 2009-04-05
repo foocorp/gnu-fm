6618f3d35bed12544d51711bbda3033b12d8d4ce
elleo <>
2009-04-05 21:54:15 +0000

    Add new lines to the end of all status messages to clients

--- gobbler/submissions/1.2/handshake.php
+++ gobbler/submissions/1.2/handshake.php
@@ -8,13 +8,13 @@ $supported_protocols = array("1.2", "1.2.1");
 
 
 if(!isset($_GET['p']) || !isset($_GET['u']) || !isset($_GET['t']) || !isset($_GET['a'])) {
-	die("BADAUTH");
+	die("BADAUTH\n");
 }
 
 $protocol = $_GET['p']; $username = $_GET['u']; $timestamp = $_GET['t']; $auth_token = $_GET['a'];
 
 if(!in_array($protocol, $supported_protocols))  {
-	die("FAILED Unsupported protocol version");
+	die("FAILED Unsupported protocol version\n");
 }
 
 if(isset($_GET['api_key']) && isset($_GET['sk'])) {
@@ -24,7 +24,7 @@ if(isset($_GET['api_key']) && isset($_GET['sk'])) {
 }
 
 if(!$authed) {
-	die("BADAUTH");
+	die("BADAUTH\n");
 }
 
 $session_id = md5($auth_token . time());
@@ -34,7 +34,7 @@ $res = $mdb2->query("INSERT INTO Scrobble_Sessions(username, sessionid, expires)
 	. $mdb2->quote(time() + 86400) . ")");
 
 if(PEAR::isError($res)) {
-	die("FAILED " . $res->getMessage());
+	die("FAILED " . $res->getMessage() . "\n");
 }
 
 echo "OK\n";
