a774ce3d43166d9836b29b6ec942a83fae794d5d
mattl <>
2009-04-05 20:50:50 +0000

    fixed HTML errors

--- gobbler/display.php
+++ gobbler/display.php
@@ -1,6 +1,22 @@
 <?php
 require_once('database.php');
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+ "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<!--
+<rdf:RDF xmlns="http://web.resource.org/cc/"
+         xmlns:dc="http://purl.org/dc/elements/1.1/"
+         xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+<license rdf:resource="http://creativecommons.org/licenses/by-sa/3.0/us/" />
+</Work>
+<License rdf:about="http://creativecommons.org/licenses/by-sa/3.0/us/">
+</License>
+</rdf:RDF>
+-->
 
 <html>
 	<head>
@@ -10,7 +26,7 @@ require_once('database.php');
 	<body>
 		<h1>Libre.fm Scrobble Server</h1>
 		
-		<p>This site handles <em>scrobble</em> and <em>now playing</em> submissions from client applications and offers access to our web services API. If you just want to use <a href="http://libre.fm">libre.fm</a> then you probably want to visit the <a href="http://libre.fm">main site</a>, but if you're a developer then here are some useful stats to help you see what's happening.
+		<p>This site handles <em>scrobble</em> and <em>now playing</em> submissions from client applications and offers access to our web services API. If you just want to use <a href="http://libre.fm">libre.fm</a> then you probably want to visit the <a href="http://libre.fm">main site</a>, but if you're a developer then here are some useful stats to help you see what's happening.</p>
 
 		<h2>Last 10 Scrobbles</h2>
 
