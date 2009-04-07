f242598936665fe507df54891d3c33ad5d471ed4
mattl <>
2009-04-07 02:44:41 +0000

    fix

--- gobbler/display.php
+++ gobbler/display.php
@@ -39,8 +39,7 @@ require_once('utils/human-time.php');
    GNU Affero General Public License for more details.
 
    You should have received a copy of the GNU Affero General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+   along with this program.  If not, see http://www.gnu.org/licenses/
 
 -->
 </head>
