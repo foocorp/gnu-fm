e790d2e46ff5555300e8aaa621726f63518ecef6
mattl <>
2009-04-07 02:46:57 +0000

    this file needs a license adding back to it

--- gobbler/display.php
+++ gobbler/display.php
@@ -1,5 +1,4 @@
 <?php
-
 header('Content-type: text/html; charset=utf-8');
 require_once('database.php');
 require_once('utils/human-time.php');
@@ -22,27 +21,6 @@ require_once('utils/human-time.php');
 </License>
 </rdf:RDF>
 -->
-<!-- //
-
-   Libre.fm -- a free network service for sharing your music listening habits
-
-   Copyright (C) 2009 Libre.fm Project
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Affero General Public License for more details.
-
-   You should have received a copy of the GNU Affero General Public License
-   along with this program.  If not, see http://www.gnu.org/licenses/
-
--->
-
 </head>
 <body>
 <div id="doc3" class="yui-t7">
