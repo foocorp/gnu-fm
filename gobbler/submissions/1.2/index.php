a567553844d3cbd996a6155a20fd0a1c6a0f961f
clint <>
2009-04-07 03:16:34 +0000

    let's try to avoid breaking the protocol by spewing license texts at the clients, shall we?

--- gobbler/submissions/1.2/index.php
+++ gobbler/submissions/1.2/index.php
@@ -1,3 +1,4 @@
+<?
 /* Libre.fm -- a free network service for sharing your music listening habits
 
    Copyright (C) 2009 Libre.fm Project
@@ -17,7 +18,6 @@
 
 */
 
-<?
 require_once('../../database.php');
 require_once('../../scrobble-utils.php');
 
