397ad9c629097fe1c0e1cd341707f78fd2a45e18
mattl <>
2009-04-08 23:00:42 +0000

    fixed script

--- scripts/users_dump.sh
+++ scripts/users_dump.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-USERNAME=
-DATABASE=
-HOST=
-PASSWORD=
+USERNAME=turtle
+DATABASE=turtle
+HOST=localhost
+PASSWORD=turtle
 
 LIST=$(mysql --skip-column-names -h $HOST -u $USERNAME -p$PASSWORD $DATABASE < users.sql)
 
-for I in $LIST; do 
+for I in $LIST; do
+
+    cat /home/librefm/scripts/license > /home/librefm/turtle/data/$I.dump
     
-    mysqldump -h $HOST -u $USERNAME -p$PASSWORD $DATABASE Scrobbles "--where=(username='$I')" > $I.dump
+    mysqldump -h $HOST -u $USERNAME -p$PASSWORD  $DATABASE Scrobbles "--where=(username='$I')" >> /home/librefm/turtle/data/$I.dump 
 done
 
