144aedd73a500181edc318fec9efd7cfb760843b
jurgbohn <>
2009-04-08 21:54:24 +0000

    Nightly build now also dumps database (Artist, Album, Track and Scrobbles-tables)

--- scripts/nbuild.sh
+++ scripts/nbuild.sh
@@ -12,12 +12,14 @@ if [ ! -d $BASEDIR ] ; then
 fi
 
 BASEDIR=$(cd "$BASEDIR" && pwd)
-
+DATE=$(date '+%Y-%m-%d')
 svn export svn://svn.savannah.nongnu.org/librefm "$BASEDIR"/librefm > /dev/null
+cd "$BASEDIR" && mysqldump -h <host> -u <user> -p<password> <database> Artist Album Track Scrobbles >database.dump && gzip -c database.dump > librefm_database_"$DATE".gz
+
 
 I=$(cd "$BASEDIR"/librefm/trunk/ && grep -c "GNU Affero General Public License" `find . -name "*.php"` --exclude="*Email*"| grep :0 | wc -l)
 if [ $I == 0 ]; then
-	cd "$BASEDIR" && tar zcf librefm_$(date '+%Y-%m-%d').tar.gz librefm
+	cd "$BASEDIR" && tar zcf librefm_"$DATE".tar.gz librefm
 	rm -r "$BASEDIR"/librefm
 else
     echo "Tainted file!" >&2
