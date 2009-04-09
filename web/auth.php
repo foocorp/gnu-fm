4c1fae6f9e10e0453a479035843c9ec259d31069
jurgbohn <>
2009-04-09 16:51:47 +0000

    One more try :)

--- web/auth.php
+++ web/auth.php
@@ -24,15 +24,16 @@ require_once('database.php');
 session_start();
 
 if(isset($_SESSION['session_id'])) {
-	$res = $mdb2->query("SELECT username FROM Scrobble_Sessions WHERE "
+	$res = $mdb2->query("SELECT Users.username,userlevel FROM Scrobble_Sessions,Users WHERE "
 		. "sessionid = " . $mdb2->quote($_SESSION['session_id'], "text")
-	       	. " AND expires > " . time());
+	       	. " AND expires > " . time() . " AND Users.username = Scrobble_Sessions.username");
 	if(!$res->numRows()) {
 		// Session is invalid
 		unset($_SESSION['session_id']);
 	} else {
 		$logged_in = true;
 		$username = $res->fetchOne(0);
+		$userlevel = $res->fetchOne(1);
 	}
 }
 ?>
