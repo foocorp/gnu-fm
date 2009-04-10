0aa3e47cf54e1fee92e7e9be492cce2c66ff9941
clint <>
2009-04-10 17:17:38 +0000

    erm

--- web/index.php
+++ web/index.php
@@ -32,6 +32,11 @@ $smarty->assign('recenttracks', $res->fetchAll(MDB2_FETCHMODE_ASSOC));
 
 
 $res = $mdb2->query('SELECT username, artist, track, client, ClientCodes.name, ClientCodes.url from Now_Playing LEFT OUTER JOIN Scrobble_Sessions ON Now_Playing.sessionid=Scrobble_Sessions.sessionid LEFT OUTER JOIN ClientCodes ON Scrobble_Sessions.client=ClientCodes.code ORDER BY time DESC');
+
+if(PEAR::isError($res)) {
+  die($res->getMessage());
+}
+
 $data = $res->fetchAll(MDB2_FETCHMODE_ASSOC);
 foreach($data as &$i) {
     $i = sanitize($i);
