b98a02916fc786a316a27a8a3c1303353b35d540
evhan <>
2009-04-13 23:18:53 +0000

    Linked welcome.php track titles to their track.php pages under the "Now Playing" section

--- web/data/Server.php
+++ web/data/Server.php
@@ -98,9 +98,17 @@ class Server {
 		global $mdb2;
 
 		if($username) {
+<<<<<<< .mine
+			$res = $mdb2->query('SELECT username, artist, track, album, client, ClientCodes.name, ClientCodes.url, Now_Playing.mbid from Now_Playing LEFT OUTER JOIN Scrobble_Sessions ON Now_Playing.sessionid=Scrobble_Sessions.sessionid LEFT OUTER JOIN ClientCodes ON Scrobble_Sessions.client=ClientCodes.code WHERE username = ' . $mdb2->quote($username, "text") . ' ORDER BY Now_Playing.expires DESC LIMIT ' . $mdb2->quote($number, "integer"));
+=======
 			$res = $mdb2->query('SELECT username, artist, album, track, client, ClientCodes.name, ClientCodes.url, Now_Playing.mbid from Now_Playing LEFT OUTER JOIN Scrobble_Sessions ON Now_Playing.sessionid=Scrobble_Sessions.sessionid LEFT OUTER JOIN ClientCodes ON Scrobble_Sessions.client=ClientCodes.code WHERE username = ' . $mdb2->quote($username, "text") . ' ORDER BY Now_Playing.expires DESC LIMIT ' . $mdb2->quote($number, "integer"));
+>>>>>>> .r414
 		} else {
+<<<<<<< .mine
+			$res = $mdb2->query('SELECT username, artist, track, album, client, ClientCodes.name, ClientCodes.url, Now_Playing.mbid from Now_Playing LEFT OUTER JOIN Scrobble_Sessions ON Now_Playing.sessionid=Scrobble_Sessions.sessionid LEFT OUTER JOIN ClientCodes ON Scrobble_Sessions.client=ClientCodes.code ORDER BY Now_Playing.expires DESC LIMIT ' . $mdb2->quote($number, "integer"));
+=======
 			$res = $mdb2->query('SELECT username, artist, album, track, client, ClientCodes.name, ClientCodes.url, Now_Playing.mbid from Now_Playing LEFT OUTER JOIN Scrobble_Sessions ON Now_Playing.sessionid=Scrobble_Sessions.sessionid LEFT OUTER JOIN ClientCodes ON Scrobble_Sessions.client=ClientCodes.code ORDER BY Now_Playing.expires DESC LIMIT ' . $mdb2->quote($number, "integer"));
+>>>>>>> .r414
 		}
 
 		if(PEAR::isError($res)) {
@@ -118,6 +126,7 @@ class Server {
 			$row["clientstr"] = $clientstr;
 			$row["userurl"] = Server::getUserURL($row["username"]);
 			$row["artisturl"] = Server::getArtistURL($row["artist"]);
+			$row["trackurl"] = Server::getTrackURL($row['artist'], $row['album'], $row['track']);
 			$result[] = $row;
 		}
 
