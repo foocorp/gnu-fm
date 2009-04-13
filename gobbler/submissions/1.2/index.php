9a8686211ae5a3b3303ea92839d3c57bf58c7c95
jurgbohn <>
2009-04-13 14:44:08 +0000

    Fixed syntaxerrors.

--- gobbler/submissions/1.2/index.php
+++ gobbler/submissions/1.2/index.php
@@ -42,7 +42,7 @@ for($i = 0; $i < count($_POST['a']); $i++) {
 		case "UTF-8":
 			$artist = $mdb2->quote($_POST['a'][$i], "text");
 		default:
-			die "FAILED Bad encoding in artist submission $i\n"
+			die("FAILED Bad encoding in artist submission $i\n");
 		}
 	if(isset($_POST['b'][$i]) && !empty($_POST['b'])) {
 		switch (mb_detect_encoding($_POST['b'][$i])) {
@@ -51,7 +51,7 @@ for($i = 0; $i < count($_POST['a']); $i++) {
 			$album = $mdb2->quote($_POST['b'][$i], "text");
 			break;
 		default:
-			die "FAILED Bad encoding in album submission $i\n";
+			die("FAILED Bad encoding in album submission $i\n");
 		}
 	} else {
 		$album = 'NULL';
@@ -61,7 +61,7 @@ for($i = 0; $i < count($_POST['a']); $i++) {
 		case "UTF-8":
 			$track = $mdb2->quote($_POST['t'][$i], "text");
 		default:
-			die "FAILED Bad encoding in title submission $i\n"
+			die("FAILED Bad encoding in title submission $i\n");
 	}
 	if(is_numeric($_POST['i'][$i])) {
 		$time = (int) $_POST['i'][$i];
