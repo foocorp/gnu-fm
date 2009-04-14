94bece26319d81465c3dd887167f91ac7fa7d560
clint <>
2009-04-14 16:56:24 +0000

    questionable whitespace

--- scripts/import-jamendo.py
+++ scripts/import-jamendo.py
@@ -202,15 +202,12 @@ class JamendoImport:
 
 						if self.track_exists(artist["name"], album["name"], track["name"]):
 							try:
-								self.cursor.execute("UPDATE Track SET downloadurl = %s, streamurl = %s, mbid = %s, license = %s, duration = %s, otherid = %s WHERE name = %s AND artist = %s AND album = %s",
-										(track["downloadurl"], track["streamurl"], track["mbid"], track["license"], duration, otherid,
-										track["name"], artist["name"], album["name"]))
+								self.cursor.execute("UPDATE Track SET downloadurl = %s, streamurl = %s, mbid = %s, license = %s, duration = %s, otherid = %s WHERE name = %s AND artist = %s AND album = %s", (track["downloadurl"], track["streamurl"], track["mbid"], track["license"], duration, otherid, track["name"], artist["name"], album["name"]))
 							except Exception,  e:
 								pass
 						else:
 							try:
-								self.cursor.execute("INSERT INTO Track (name, artist, album, mbid, downloadurl, streamurl, license, duration, otherid) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-										(track["name"], artist["name"], album["name"], track["mbid"], track["downloadurl"], track["streamurl"], track["license"], duration, otherid))
+								self.cursor.execute("INSERT INTO Track (name, artist, album, mbid, downloadurl, streamurl, license, duration, otherid) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (track["name"], artist["name"], album["name"], track["mbid"], track["downloadurl"], track["streamurl"], track["license"], duration, otherid))
 							except Exception,  e:
 								pass
 
