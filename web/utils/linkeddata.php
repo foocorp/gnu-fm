39d39f44a7b2443bee77ddf55ce5348a38d2793e
tobyink <>
2009-04-15 15:11:40 +0000

    Add artist MusicBrainz IDs.

--- web/utils/linkeddata.php
+++ web/utils/linkeddata.php
@@ -1,13 +1,13 @@
 <?php
 
-function identifierScrobbleEvent ($username, $artist, $track, $time, $mbid=NULL)
+function identifierScrobbleEvent ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL)
 {
 	global $base_url;
 	$microhash = substr(md5($artist . '//' . $track), 0, 4);
 	return $base_url . sprintf('/user/%s#%s.%s', urlencode($username), urlencode($time), urlencode($microhash));
 }
 
-function identifierTrack ($username, $artist, $track, $time, $mbid=NULL)
+function identifierTrack ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL)
 {
 	if (!empty($mbid))
 	{
@@ -15,13 +15,20 @@ function identifierTrack ($username, $artist, $track, $time, $mbid=NULL)
 	}
 	else
 	{
-		return identifierScrobbleEvent($username, $artist, $track, $time, $mbid) . '.track';
+		return identifierScrobbleEvent($username, $artist, $track, $time, $mbid, $ambid) . '.track';
 	}
 }
 
-function identifierArtist ($username, $artist, $track, $time, $mbid=NULL)
+function identifierArtist ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL)
 {
 	# Eventually look up MBIDs from Artists table?
-	return identifierScrobbleEvent($username, $artist, $track, $time, $mbid) . '.artist';
+	if (!empty($ambid))
+	{
+		return sprintf('http://dbtune.org/musicbrainz/resource/artist/%s', strtolower($ambid));
+	}
+	else
+	{
+		return identifierScrobbleEvent($username, $artist, $track, $time, $mbid, $ambid) . '.artist';
+	}
 }
 
