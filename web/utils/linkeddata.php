c577f91695a2d6c459a5b6c04aadddbe730616de
tobyink <>
2009-04-16 08:54:49 +0000

    Add album art to profile pages.

--- web/utils/linkeddata.php
+++ web/utils/linkeddata.php
@@ -1,13 +1,13 @@
 <?php
 
-function identifierScrobbleEvent ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL)
+function identifierScrobbleEvent ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL, $lmbid=NULL)
 {
 	global $base_url;
 	$microhash = substr(md5($artist . '//' . $track), 0, 4);
 	return $base_url . sprintf('/user/%s#%s.%s', urlencode($username), urlencode($time), urlencode($microhash));
 }
 
-function identifierTrack ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL)
+function identifierTrack ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL, $lmbid=NULL)
 {
 	if (!empty($mbid))
 	{
@@ -15,11 +15,11 @@ function identifierTrack ($username, $artist, $track, $time, $mbid=NULL, $ambid=
 	}
 	else
 	{
-		return identifierScrobbleEvent($username, $artist, $track, $time, $mbid, $ambid) . '.track';
+		return identifierScrobbleEvent($username, $artist, $track, $time, $mbid, $ambid, $lmbid) . '.track';
 	}
 }
 
-function identifierArtist ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL)
+function identifierArtist ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL, $lmbid=NULL)
 {
 	# Eventually look up MBIDs from Artists table?
 	if (!empty($ambid))
@@ -28,7 +28,20 @@ function identifierArtist ($username, $artist, $track, $time, $mbid=NULL, $ambid
 	}
 	else
 	{
-		return identifierScrobbleEvent($username, $artist, $track, $time, $mbid, $ambid) . '.artist';
+		return identifierScrobbleEvent($username, $artist, $track, $time, $mbid, $ambid, $lmbid) . '.artist';
+	}
+}
+
+function identifierAlbum ($username, $artist, $track, $time, $mbid=NULL, $ambid=NULL, $lmbid=NULL)
+{
+	# Eventually look up MBIDs from Artists table?
+	if (!empty($lmbid))
+	{
+		return sprintf('http://dbtune.org/musicbrainz/resource/record/%s', strtolower($lmbid));
+	}
+	else
+	{
+		return identifierScrobbleEvent($username, $artist, $track, $time, $mbid, $ambid, $lmbid) . '.album';
 	}
 }
 
