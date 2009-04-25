d1a258fee2d2d0b1f19200a4570eb7f0807e17f5
clint <>
2009-04-25 14:26:34 +0000

    avoid periods of empty dump files in data/ directory

--- scripts/users_dump.sh
+++ scripts/users_dump.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /home/librefm/turtle/data/ || exit 1
+cd /home/librefm/turtle/data/wip || exit 1
 
 #we rely on lack of whitespace here
 LIST=$(echo "SELECT DISTINCT username FROM Users;" | psql -q -t)
@@ -10,4 +10,6 @@ for I in $LIST; do
     cat /home/librefm/scripts/license > $I.text.utf8
     
     echo 'COPY (SELECT * FROM Scrobbles where username='"'$I'"') TO STDOUT WITH CSV HEADER;' | psql -q >> $I.text.utf8
+
+    mv -- $I /home/librefm/turtle/data/
 done
