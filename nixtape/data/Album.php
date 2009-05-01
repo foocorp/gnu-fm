5b1b238eac0ef668d08fcf78d9d2aefadc8d4038
mattl <>
2009-05-01 16:04:19 +0000

    Added copyright notices on newly assigned files.

--- nixtape/data/Album.php
+++ nixtape/data/Album.php
@@ -2,7 +2,7 @@
 
 /* Libre.fm -- a free network service for sharing your music listening habits
 
-   Copyright (C) 2009 Libre.fm Project
+   Copyright (C) 2009 Free Software Foundation, Inc
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Affero General Public License as published by
