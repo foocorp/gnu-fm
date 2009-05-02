0a2a58010024c3951e4db859f3737b01e5021360
clint <>
2009-05-02 01:37:12 +0000

    perms

--- gnukebox/install.php
+++ gnukebox/install.php
@@ -249,9 +249,9 @@ if (isset($_POST['install'])) {
 //      $res = $mdb2->exec("CREATE INDEX scrobbles_track_idx on Scrobbles(lower(track))");
 
 // uncomment these if you're using postgresql and want to run the software as www-data
-//	$res = $mdb2->exec("GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE Album, Artist, Auth, Clientcodes, Error, Invitation_Request, Invitations, Now_Playing, Places, Radio_Sessions, Scrobble_Sessions, Scrobbles, Similar_artist, Tags, Track, Users to \"www-data\"");
+//	$res = $mdb2->exec("GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE Album, Artist, Auth, Clientcodes, Error, Invitation_Request, Invitations, Now_Playing, Places, Radio_Sessions, Scrobble_Sessions, Scrobbles, Scrobble_Track, Similar_Artist, Tags, Track, Users to \"www-data\"");
 //	$res = $mdb2->exec("GRANT SELECT ON Free_Scrobbles to \"www-data\"");
-//	$res = $mdb2->exec("GRANT SELECT, UPDATE ON users_uniqueid_seq to \"www-data\"");
+//	$res = $mdb2->exec("GRANT SELECT, UPDATE ON users_uniqueid_seq, scrobble_track_id_seq to \"www-data\"");
 
 	// Test user configuration
 	$res = $mdb2->query("INSERT INTO Users
