faa2eb45e4cd0f8e86b4c8542488097a8547102c
tobyink <>
2009-05-02 08:29:33 +0000

    Provide better rel=meta link for /user/$username/groups page. Provide a rel=meta link for /user/$username/stats page.

--- nixtape/user-groups.php
+++ nixtape/user-groups.php
@@ -54,7 +54,7 @@ if(isset($user->name)) {
 				'rel' => 'meta',
 				'type' => 'application/rdf+xml' ,
 				'title' => 'FOAF',
-				'href' => $base_url.'/rdf.php?fmt=xml&page='.htmlentities(str_replace($base_url, '', $user->getURL()))
+				'href' => $base_url.'/rdf.php?fmt=xml&page='.htmlentities(str_replace($base_url, '', $user->getURL('groups')))
 				)
 		));
 		
