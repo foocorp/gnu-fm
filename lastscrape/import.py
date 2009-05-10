87b6a91e1bce0618f17f21cd7d62d8b5927dcf7e
Daniel Watkins <daniel@daniel-watkins.co.uk>
2009-05-10 15:57:39 +0100

    Make libre.fm server default for import.py.

--- lastscrape/import.py
+++ lastscrape/import.py
@@ -6,21 +6,21 @@ sys.path.append(os.path.join(sys.path[0], '../scripts'))
 
 from datetime import datetime
 import getpass
-from gobble import GobbleServer, GobbleTrack
-from optparse import OptionParser
+from gobble import get_parser, GobbleServer, GobbleTrack
 import time
 from urllib import urlencode
 from urllib2 import urlopen
 
 
 if __name__ == '__main__':
-    usage = "%prog <USERNAME> <SERVER> <SCROBBLE DUMP>"
-    parser = OptionParser(usage=usage)
+    usage = "%prog [-s <SERVER>] <USERNAME> <SCROBBLE DUMP>"
+    parser = get_parser(usage=usage)
     opts,args = parser.parse_args()
-    if len(args) != 3:
+    if len(args) != 2:
         parser.error("All arguments are required.")
 
-    username,server,data = args
+    username,data = args
+    server = opts.server
     password = getpass.getpass()
     gobbler = GobbleServer(server, username, password)
 
