fb002021ff8ea11c9626b459f4680e492a01d390
Daniel Watkins <daniel@daniel-watkins.co.uk>
2009-05-10 16:00:52 +0100

    Made libre.fm server default for most_recent.py.

--- scripts/most_recent.py
+++ scripts/most_recent.py
@@ -5,16 +5,19 @@ from optparse import OptionParser
 from urllib2 import urlopen
 
 from BeautifulSoup import BeautifulSoup
+from gobble import get_parser
 
 
 if __name__ == '__main__':
-    usage = "%prog <USERNAME> <WEB SERVER>"
-    parser = OptionParser(usage=usage)
+    usage = "%prog [-s <WEB SERVER>] <USERNAME>"
+    parser = get_parser(usage=usage)
+    parser.set_defaults(server='alpha.libre.fm')
     opts,args = parser.parse_args()
-    if len(args) != 2:
+    if len(args) != 1:
         parser.error("All arguments are required.")
 
-    username,server = args
+    username, = args
+    server = opts.server
     if server[:7] != "http://":
         server = "http://%s" % (server,)
     soup = BeautifulSoup(urlopen('%s/user/%s/recent-tracks'
