1456bef845a5b6ecb4a9185d840b9c878f26fdbb
elleo <>
2009-05-10 12:21:11 +0000

    Fix more inconsistent indentation

--- nixtape/data/TagCloud.php
+++ nixtape/data/TagCloud.php
@@ -23,47 +23,48 @@ require_once($install_path . '/data/Server.php');
 require_once($install_path . '/config.php'); // Should already be required though.
 
 class TagCloud {
-   /*
-    * returns an array counting appareances of a given field and his corresponding font-size.
-    * @param string $table table name to be queried
-    * @param string $field field name to count
-    * @param integer $limit limit of the query
-    * @param string $constraint username or artistname depending on field
-    * inaccurate @param integer $sizes quantity of possible sizes
-    * inaccurate @param float $max_font_size maximum font size (px, em, %, etc)
-    * @return array tagcloud
-    */
-    static function generateTagCloud($table, $field, $limit = 40, $constraint = null, $constrained_field = false) {
-        global $adodb;
-        if (!is_string($field))          return false;	
-        if (!is_string($table))          return false;
-        if (!is_integer($limit))         return false;
-        $sizes = array('xx-large', 'x-large', 'large', 'medium', 'small', 'x-small', 'xx-small');
-        $query = "SELECT $field, count(*) AS count FROM $table";
-        $query .= (!is_null($constraint)) ? ' WHERE ' : null;
-        if ($constrained_field) {
-            $query .= (!is_null($constraint)) ? " $constrained_field  = " . $adodb->qstr($constraint) : null;
-        } elseif ($field == "track") {
-            $query .= (!is_null($constraint)) ? ' artist = ' . $adodb->qstr($constraint) : null;
-        } else {
-            $query .= (!is_null($constraint)) ? ' username = ' . $adodb->qstr($constraint) : null;
-        }
-        $query .= " GROUP BY $field ORDER BY count DESC LIMIT $limit";
-	$adodb->SetFetchMode(ADODB_FETCH_ASSOC);
-        $res = $adodb->CacheGetAll(7200,$query);
-        if (!$res) {
-            echo("ERROR $query");
-        } else {
-            foreach($res as $count => &$i) {
-                $i['size'] = $sizes[(int) ($count/(count($res)/7))];
-            }
-            foreach($res as &$i){
-                $i['pageurl'] = Server::getArtistURL($i['artist']);
-            }
-            sort($res);
-            return $res;
-        }
-    }
+	
+	/*
+	 * returns an array counting appareances of a given field and his corresponding font-size.
+	 * @param string $table table name to be queried
+	 * @param string $field field name to count
+	 * @param integer $limit limit of the query
+	 * @param string $constraint username or artistname depending on field
+	 * inaccurate @param integer $sizes quantity of possible sizes
+	 * inaccurate @param float $max_font_size maximum font size (px, em, %, etc)
+	 * @return array tagcloud
+	 */
+	static function generateTagCloud($table, $field, $limit = 40, $constraint = null, $constrained_field = false) {
+		global $adodb;
+		if (!is_string($field))          return false;	
+		if (!is_string($table))          return false;
+		if (!is_integer($limit))         return false;
+		$sizes = array('xx-large', 'x-large', 'large', 'medium', 'small', 'x-small', 'xx-small');
+		$query = "SELECT $field, count(*) AS count FROM $table";
+		$query .= (!is_null($constraint)) ? ' WHERE ' : null;
+		if ($constrained_field) {
+			$query .= (!is_null($constraint)) ? " $constrained_field  = " . $adodb->qstr($constraint) : null;
+		} elseif ($field == "track") {
+			$query .= (!is_null($constraint)) ? ' artist = ' . $adodb->qstr($constraint) : null;
+		} else {
+			$query .= (!is_null($constraint)) ? ' username = ' . $adodb->qstr($constraint) : null;
+		}
+		$query .= " GROUP BY $field ORDER BY count DESC LIMIT $limit";
+		$adodb->SetFetchMode(ADODB_FETCH_ASSOC);
+		$res = $adodb->CacheGetAll(7200,$query);
+		if (!$res) {
+			echo("ERROR $query");
+		} else {
+			foreach($res as $count => &$i) {
+				$i['size'] = $sizes[(int) ($count/(count($res)/7))];
+			}
+			foreach($res as &$i){
+				$i['pageurl'] = Server::getArtistURL($i['artist']);
+			}
+			sort($res);
+			return $res;
+		}
+    	}
     
 	/**
 	 * Returns the preferred table to generate scrobble data from.
