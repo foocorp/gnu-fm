f6a716a321804cb66d211a92b79802ebbc6c4dee
clint <>
2009-05-14 03:49:51 +0000

    convert radio/xspf.php to adodb

--- nixtape/radio/xspf.php
+++ nixtape/radio/xspf.php
@@ -18,7 +18,7 @@
 
 */
 
-require_once('../database.php');
+require_once('../database2.php');
 require_once('../templating.php');
 require_once('../data/Track.php');
 require_once('radio-utils.php');
@@ -31,44 +31,44 @@ if(!isset($_GET['sk']) || !isset($_GET['desktop'])) {
 
 $session = $_GET['sk'];
 
-$res = $mdb2->query('SELECT url FROM Radio_Sessions WHERE session = ' . $mdb2->quote($session, 'text'));
+$res = $adodb->GetOne('SELECT url FROM Radio_Sessions WHERE session = ' . $adodb->qstr($session));
 
-if(!$res->numRows()) {
+if(!$res) {
 	die("BADSESSION\n"); // this should return a blank dummy playlist instead
 }
 
-$url = $res->fetchOne(0);
+$url = $res;
 
 $title = radio_title_from_url($url);
 $smarty->assign('title', $title);
 
 if(ereg('l(ast|ibre)fm://globaltags/(.*)', $url, $regs)) {
 	$tag = $regs[2];
-	$res = $mdb2->query('SELECT Track.name, Track.artist_name, Track.album_name FROM Track INNER JOIN Tags ON Track.name=Tags.track AND Track.artist_name=Tags.artist AND Track.album_name=Tags.album WHERE streamurl<>\'\' AND streamable=1 AND lower(tag) = ' . $mdb2->quote(mb_strtolower($tag, 'UTF-8'), 'text'));
+	$res = $adodb->Execute('SELECT Track.name, Track.artist_name, Track.album_name FROM Track INNER JOIN Tags ON Track.name=Tags.track AND Track.artist_name=Tags.artist AND Track.album_name=Tags.album WHERE streamurl<>\'\' AND streamable=1 AND lower(tag) = ' . $adodb->qstr(mb_strtolower($tag, 'UTF-8')));
 } elseif(ereg('l(ast|ibre)fm://artist/(.*)/similarartists', $url, $regs)) {
 	$artist = $regs[2];
-	$res = $mdb2->query('SELECT name, artist_name, album_name FROM Track WHERE streamurl<>\'\' AND streamable=1 AND lower(artist_name) = ' . $mdb2->quote(mb_strtolower($artist, 'UTF-8'), 'text'));
+	$res = $adodb->Execute('SELECT name, artist_name, album_name FROM Track WHERE streamurl<>\'\' AND streamable=1 AND lower(artist_name) = ' . $adodb->qstr(mb_strtolower($artist, 'UTF-8')));
 } else {
 	die("FAILED\n"); // this should return a blank dummy playlist instead
 }
 
-
-$avail = $res->numRows();
+$avail = $res->RecordCount();
 
 // This needs some kind of deduplication among other things
-$tr[0] = rand(1,$avail);
-$tr[1] = rand(1,$avail);
-$tr[2] = rand(1,$avail);
-$tr[3] = rand(1,$avail);
-$tr[4] = rand(1,$avail);
+$tr[0] = rand(0,$avail-1);
+$tr[1] = rand(0,$avail-1);
+$tr[2] = rand(0,$avail-1);
+$tr[3] = rand(0,$avail-1);
+$tr[4] = rand(0,$avail-1);
 sort($tr);
 
 $radiotracks = array();
+$adodb->SetFetchMode(ADODB_FETCH_ASSOC);
 
 	for($i=0; $i<5; $i++) {
 
-	$res->seek($tr[$i]);
-	$row = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
+	$res->Move($tr[$i]);
+	$row = $res->GetRow();
 
 	$track = new Track($row['name'], $row['artist_name']);
 	$album = new Album($row['album_name'], $row['artist_name']);
