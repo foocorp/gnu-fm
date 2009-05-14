5083289194125cea7205c88cc3339818ce8a9441
mattl <>
2009-05-14 19:37:44 +0000

    wtf is this stuff doing?

--- nixtape/data/Group.php
+++ nixtape/data/Group.php
@@ -283,7 +283,7 @@ class Group {
 		global $base_uri;
 		if (!empty($this->avatar_uri))
 			return $this->avatar_uri;
-		return $base_uri . '/themes/' . $default_theme . '/images/default-avatar.stream.png';
+		return $base_url . '/themes/' . $default_theme . '/images/default-avatar.stream.png';
 	}
 
 	function getURL() {
