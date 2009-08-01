cb3564347e5f6090452b949a8370fc74ded52a9b
Mike Sheldon <mike@mikeasoft.com>
2009-08-01 22:22:46 +0100

    Add first stab at a streaming client for the Android mobile platform

---
+++ clients/libredroid/src/fm/libre/droid/Song.java
@@ -0,0 +1,52 @@
+/************************************************************************\
+*                                                                       *
+* Libre Droid - A libre.fm streaming music client for the Android       *
+* mobile platform                                                       *
+* Copyright (C) 2009  Michael Sheldon <mike@mikeasoft.com>              *
+*                                                                       *
+* This program is free software: you can redistribute it and/or modify  *
+* it under the terms of the GNU General Public License as published by  *
+* the Free Software Foundation, either version 3 of the License, or     *
+* (at your option) any later version.                                   *
+*                                                                       *
+* This program is distributed in the hope that it will be useful,       *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+* GNU General Public License for more details.                          *
+*                                                                       *
+* You should have received a copy of the GNU General Public License     *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+*                                                                       *
+*************************************************************************/
+
+package fm.libre.droid;
+
+public class Song {
+
+	public String title;
+	public String location;
+	public String album;
+	public String artist;
+	public String imageURL;
+	public String artistURL;
+	public String albumURL;
+	public String trackURL;
+	public String downloadURL;
+	
+	Song() {
+		this.title = "";
+		this.location = "";
+		this.album = "";
+		this.artist = "";
+		this.imageURL = "";
+		this.artistURL = "";
+		this.albumURL = "";
+		this.trackURL = "";
+		this.downloadURL = "";
+	}
+	
+	public String toString() {
+		return this.artist + " - " + this.title;
+	}
+	
+}
