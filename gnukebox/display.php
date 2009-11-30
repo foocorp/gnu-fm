05523a270078b161312f9ee6793750f5aa863dfd
Matt Lee <mattl@mattl.libre.fm>
2009-11-29 19:50:32 -0500

    remove all librefm branding

--- gnukebox/display.php
+++ gnukebox/display.php
@@ -27,20 +27,11 @@ require_once('temp-utils.php');
 <!DOCTYPE html>
 <html>
 <head>
-   <title>Libre.fm server version <?php echo $version; ?></title>
+   <title>GNUkebox</title>
    <meta name="author" content="FooCorp catalogue number FOO200 and contributors" />
-   <link rel="stylesheet" href="http://s.libre.fm/librefm/css/r.css" type="text/css" />
-   <link rel="stylesheet" href="http://s.libre.fm/librefm/css/b.css" type="text/css" />
 <meta http-equiv="refresh" content="180" />
 </head>
 <body>
-<div id="doc2" class="yui-t6">
-   <div id="hd" role="banner">
-   <h1><a href="http://libre.fm/"><img src="http://s.libre.fm/librefm/img/lfm-top-black.png" alt="Libre.fm" /></a></h1></div>
-   <div id="bd" role="main">
-	<div id="yui-main">
-	<div class="yui-b"><div class="yui-g">
-
        <h2>Last 25 tracks received</h2>
 
      <?php
@@ -68,20 +59,8 @@ require_once('temp-utils.php');
 			?>
 
 	</ul>
-</div>
-</div>
-	</div>
-	<div class="yui-b">
 
-	  <h2>Libre.fm status updates</h2>
+<p>This server is powered by <a href="http://bzr.savannah.gnu.org/lh/librefm/">GNU FM</a> version <?php echo $version; ?></p>
 
-	    <p>Nothing to report at this time.</p>
-
-        <p>Subscribe to updates <a href="http://identi.ca/librefm">at identi.ca</a>.</p>
-
-        </div>
-
-</div>
-  <div id="ft" role="contentinfo"><p>Libre.fm is powered by <a href="http://bzr.savannah.gnu.org/lh/librefm/">GNU FM</a></p></div>
 </body>
 </html>
