d414d5f728c94ca8bbbe86dd060580b977b12383
Clint Adams <schizo@debian.org>
2009-12-18 17:22:14 -0500

    explicitly set $user to NULL when an exception is caught

--- nixtape/user-stats.php
+++ nixtape/user-stats.php
@@ -33,13 +33,14 @@ if(!isset($_GET['user']) && $logged_in == false) {
 }
 
 try {
-$user = new User($_GET['user']);
+    $user = new User($_GET['user']);
 } catch (Exception $e) {
     if ($e->getCode() == 22) {
        echo('We had some trouble locating that user.  Are you sure you spelled it correctly?'."\n");
     } else {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
     }
+    $user = NULL;
 }
 
 if(isset($user->name)) {
