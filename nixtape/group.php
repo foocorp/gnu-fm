055b297ccedcc7918786423de9200455d9422553
Matt Lee <mattl@cnuk.org>
2010-02-05 15:27:13 -0500

    disabling groups until sql is sane

--- nixtape/group.php
+++ nixtape/group.php
@@ -25,78 +25,80 @@ require_once('data/Group.php');
 require_once('data/TagCloud.php');
 require_once('data/Server.php');
 
-if (! $_GET['group'])
-{
-	$smarty->assign('groups', Group::groupList());
-	$smarty->assign('extra_head_links', array(
-				array(
-					'rel' => 'meta',
-					'type' => 'application/rdf+xml' ,
-					'title' => 'FOAF',
-					'href' => $base_url.'/rdf.php?fmt=xml&page='.urlencode(str_replace($base_url, '', $_SERVER['REQUEST_URI']))
-				     )
-				));
-	try {
-		$aTagCloud = TagCloud::GenerateTagCloud(TagCloud::scrobblesTable(), 'artist');
-		$smarty->assign('tagcloud', $aTagCloud);
-	} catch (exception $e) {}
-	$smarty->display('group-list.tpl');
-	exit;
-}
+?><h1>Groups are disabled</h1><?php 
 
-$group = new Group($_GET['group']);
+// if (! $_GET['group'])
+// {
+// 	$smarty->assign('groups', Group::groupList());
+// 	$smarty->assign('extra_head_links', array(
+// 				array(
+// 					'rel' => 'meta',
+// 					'type' => 'application/rdf+xml' ,
+// 					'title' => 'FOAF',
+// 					'href' => $base_url.'/rdf.php?fmt=xml&page='.urlencode(str_replace($base_url, '', $_SERVER['REQUEST_URI']))
+// 				     )
+// 				));
+// 	try {
+// 		$aTagCloud = TagCloud::GenerateTagCloud(TagCloud::scrobblesTable(), 'artist');
+// 		$smarty->assign('tagcloud', $aTagCloud);
+// 	} catch (exception $e) {}
+// 	$smarty->display('group-list.tpl');
+// 	exit;
+// }
 
-if ($_GET['action'] && isset($this_user))
-{
-	if ($_GET['action'] == 'join')
-		$group->memberJoin($this_user);
-	elseif ($_GET['action'] == 'leave')
-		$group->memberLeave($this_user);
+// $group = new Group($_GET['group']);
 
-	header('Location: ' . $group->getURL());
-	exit;
-}
+// if ($_GET['action'] && isset($this_user))
+// {
+// 	if ($_GET['action'] == 'join')
+// 		$group->memberJoin($this_user);
+// 	elseif ($_GET['action'] == 'leave')
+// 		$group->memberLeave($this_user);
+
+// 	header('Location: ' . $group->getURL());
+// 	exit;
+// }
 
 
-if(isset($group->name)) {
+// if(isset($group->name)) {
 
-	$smarty->assign('id', $group->id);
-	$smarty->assign('group', $group->name);
-	$smarty->assign('fullname', $group->fullname);
-	$smarty->assign('bio', $group->bio);
-	$smarty->assign('homepage', $group->homepage);
-	$smarty->assign('avatar', $group->getAvatar());
+// 	$smarty->assign('id', $group->id);
+// 	$smarty->assign('group', $group->name);
+// 	$smarty->assign('fullname', $group->fullname);
+// 	$smarty->assign('bio', $group->bio);
+// 	$smarty->assign('homepage', $group->homepage);
+// 	$smarty->assign('avatar', $group->getAvatar());
 
-	try {
-		$aUserTagCloud = $group->tagCloudData();
-		$smarty->assign('group_tagcloud',$aUserTagCloud);
-	} catch (exception $e) {}
+// 	try {
+// 		$aUserTagCloud = $group->tagCloudData();
+// 		$smarty->assign('group_tagcloud',$aUserTagCloud);
+// 	} catch (exception $e) {}
 
-	$smarty->assign('userlist', $group->getUsers());
+// 	$smarty->assign('userlist', $group->getUsers());
 
-	$smarty->assign('ismember', $group->memberCheck($this_user));
-	$smarty->assign('isowner', ($group->owner->name==$this_user->name));
-	$smarty->assign('link_join', $group->getURLAction('join'));
-	$smarty->assign('link_leave', $group->getURLAction('leave'));
-	$smarty->assign('link_edit', $base_url.'/edit_group.php?group='.$group->name);
-	$smarty->assign('link', $group->getURL());
+// 	$smarty->assign('ismember', $group->memberCheck($this_user));
+// 	$smarty->assign('isowner', ($group->owner->name==$this_user->name));
+// 	$smarty->assign('link_join', $group->getURLAction('join'));
+// 	$smarty->assign('link_leave', $group->getURLAction('leave'));
+// 	$smarty->assign('link_edit', $base_url.'/edit_group.php?group='.$group->name);
+// 	$smarty->assign('link', $group->getURL());
 
-	$smarty->assign('extra_head_links', array(
-				array(
-					'rel' => 'meta',
-					'type' => 'application/rdf+xml' ,
-					'title' => 'FOAF',
-					'href' => $base_url.'/rdf.php?fmt=xml&page='.urlencode(str_replace($base_url, '', $_SERVER['REQUEST_URI']))
-				     )
-				));
+// 	$smarty->assign('extra_head_links', array(
+// 				array(
+// 					'rel' => 'meta',
+// 					'type' => 'application/rdf+xml' ,
+// 					'title' => 'FOAF',
+// 					'href' => $base_url.'/rdf.php?fmt=xml&page='.urlencode(str_replace($base_url, '', $_SERVER['REQUEST_URI']))
+// 				     )
+// 				));
 
-	$smarty->assign('profile', true);
-	$smarty->display('group.tpl');
+// 	$smarty->assign('profile', true);
+// 	$smarty->display('group.tpl');
 
-} else {
-	$smarty->assign('error', 'Group not found');
-	$smarty->assign('details', 'Shall I call in a missing peoples report?');
-	$smarty->display('error.tpl');
-}
+// } else {
+// 	$smarty->assign('error', 'Group not found');
+// 	$smarty->assign('details', 'Shall I call in a missing peoples report?');
+// 	$smarty->display('error.tpl');
+// }
 
 ?>
