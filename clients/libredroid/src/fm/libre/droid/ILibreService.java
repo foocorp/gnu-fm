f8e129d183d802c16adfb7e786e48a736aa98ab0
Mike Sheldon <mike@mikeasoft.com>
2010-04-11 02:03:49 +0100

    Refactor Libre Droid into a service based application to support proper background playing and orientation changes

---
+++ clients/libredroid/src/fm/libre/droid/ILibreService.java
@@ -0,0 +1,20 @@
+package fm.libre.droid;
+
+public interface ILibreService {
+	
+	public boolean login(String username, String password);
+	public boolean isLoggedIn();
+	public boolean isPlaying();
+	public void setCurrentPage(int page);
+	public int getCurrentPage();
+	public void stop();
+	public void play();
+	public void next();
+	public void prev();
+	public Song getSong();
+	public Song getSong(int songNumber);
+	public void tuneStation(String type, String station);
+	public void togglePause();
+	public String getStationName();
+
+}
