52444f6df17e71d72477cdf6fd74419d659a4fe2
Mike Sheldon <mike@mikeasoft.com>
2010-04-13 18:35:20 +0100

    Make popular artist tag cloud available to non-logged in users as a separate page from the welcome page

---
+++ nixtape/popular-page.php
@@ -0,0 +1,28 @@
+<?php
+
+/* GNU FM -- a free network service for sharing your music listening habits
+
+   Copyright (C) 2009 Free Software Foundation, Inc
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+require_once('database.php');
+require_once('templating.php');
+require_once('data/sanitize.php');
+require_once('data/Server.php');
+
+$smarty->display('popular-page.tpl');
+?>
