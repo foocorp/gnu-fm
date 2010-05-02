802a0dee3d8876f56146cb447ab0a54830da0059
Mike Sheldon <mike@mikeasoft.com>
2010-05-02 17:08:45 +0100

    Make sure the track duration is set before reformating it

--- nixtape/track.php
+++ nixtape/track.php
@@ -59,12 +59,14 @@ foreach($res as &$trow) {
 	}
 }
 
-// Give the duration in MM:SS
-$mins = floor($track->duration / 60);
-$sec = floor($track->duration % 60);
-if (strlen($sec) == 1) { $sec = "0" . $sec; }
-$duration = $mins . ":" . $sec;
-$smarty->assign('duration', $duration);
+if($track->duration) {
+	// Give the duration in MM:SS
+	$mins = floor($track->duration / 60);
+	$sec = floor($track->duration % 60);
+	if (strlen($sec) == 1) { $sec = "0" . $sec; }
+	$duration = $mins . ":" . $sec;
+	$smarty->assign('duration', $duration);
+}
 
 $smarty->assign('albums', $aOtherAlbums);
 
