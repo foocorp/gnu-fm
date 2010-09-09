cf96ec4a13c60e3e9842b2c15b764addf8809464
Mike Sheldon <mike@mikeasoft.com>
2010-09-09 18:37:46 +0100

    Update search page to use new heading system

--- nixtape/search.php
+++ nixtape/search.php
@@ -31,6 +31,7 @@ $search_type = $_GET['search_type'];
 
 $results = Server::search($search_term, $search_type);
 
+$smarty->assign('pageheading', 'Results for: ' . $search_term);
 $smarty->assign('search_term', $search_term);
 $smarty->assign('search_type', $search_type);
 $smarty->assign('results', $results);
