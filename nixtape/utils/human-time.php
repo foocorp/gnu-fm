d363a2ff67adbdd4e1b10a0cd44218f10e9bdbfb
Mike Sheldon <mike@mikeasoft.com>
2011-03-14 23:27:54 +0000

    Fix formatting of timestamp humanising util

--- nixtape/utils/human-time.php
+++ nixtape/utils/human-time.php
@@ -41,68 +41,68 @@
 		);
 
 		switch ($unix_timestamp) {
-		case ($now < $unix_timestamp):
-			return 'in the future (?)';
-			break;
-		case ($diff == 1):
-			# one second
-			return 'a second ago';
-			break;
-		case ($diff < 60):
-			# less than a minute
-			return $diff . ' seconds ago';
-			break;
-		case ($diff < 120):
-			# between a minute and two
-			return 'about a minute ago';
-			break;
-		case ($diff < 3600):
-			# less than an hour
-			return round($diff / 60) . ' minutes ago';
-			break;
-		case ($diff < 7200):
-			# between an hour and two
-			return 'about an hour ago';
-			break;
-		case ($diff < 86400):
-			# less than a day
-			return round($diff / 3600) . ' hours ago';
-			break;
-		case ($diff < 172800):
-			# less than two days
-			return 'about a day ago';
-			break;
-		case ($diff < 604800):
-			# less than a week
-			if (round($diff / 86400) == 7)
+			case ($now < $unix_timestamp):
+				return 'in the future (?)';
+				break;
+			case ($diff == 1):
+				# one second
+				return 'a second ago';
+				break;
+			case ($diff < 60):
+				# less than a minute
+				return $diff . ' seconds ago';
+				break;
+			case ($diff < 120):
+				# between a minute and two
+				return 'about a minute ago';
+				break;
+			case ($diff < 3600):
+				# less than an hour
+				return round($diff / 60) . ' minutes ago';
+				break;
+			case ($diff < 7200):
+				# between an hour and two
+				return 'about an hour ago';
+				break;
+			case ($diff < 86400):
+				# less than a day
+				return round($diff / 3600) . ' hours ago';
+				break;
+			case ($diff < 172800):
+				# less than two days
+				return 'about a day ago';
+				break;
+			case ($diff < 604800):
+				# less than a week
+				if (round($diff / 86400) == 7)
+					return 'about a week ago';
+				return round($diff / 86400) . ' days ago';
+				break;
+			case ($diff < 691200):
+				# a week an a day
 				return 'about a week ago';
-			return round($diff / 86400) . ' days ago';
-			break;
-		case ($diff < 691200):
-			# a week an a day
-			return 'about a week ago';
-			break;
-		case ($diff < 2764800):
-			# less than a month
-			if (round($diff / 691200) == 1)
-				return 'about a week ago';
-			return round($diff / 691200) . ' weeks ago';
-			break;
-		case ($diff < 4579200):
-			# a month and three weeks
-			return 'about a month ago';
-			break;
-		case ($diff < 33177600);
-			# less than a year
-			return round($diff / 2764800) . ' months ago';
-			break;
-		case ($diff < 35942400):
-			# a year and a month
-			return 'about a year ago';
-			break;
-		case ($diff > 35942400):
-			return 'more than a year ago';
-			break;
+				break;
+			case ($diff < 2764800):
+				# less than a month
+				if (round($diff / 691200) == 1)
+					return 'about a week ago';
+				return round($diff / 691200) . ' weeks ago';
+				break;
+			case ($diff < 4579200):
+				# a month and three weeks
+				return 'about a month ago';
+				break;
+			case ($diff < 33177600);
+				# less than a year
+				return round($diff / 2764800) . ' months ago';
+				break;
+			case ($diff < 35942400):
+				# a year and a month
+				return 'about a year ago';
+				break;
+			case ($diff > 35942400):
+				return 'more than a year ago';
+				break;
 		}
 	}
 ?>
