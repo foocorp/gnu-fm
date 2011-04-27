c40765466a9c6dfb801968795f9a29f02aa6c767
P. J. McDermott <pjm@nac.net>
2011-04-26 21:18:26 -0400

    Finish code cleanup in nixtape/utils/.

--- nixtape/utils/licenses.php
+++ nixtape/utils/licenses.php
@@ -1,15 +1,15 @@
 <?php
-define('BY1',    1);
-define('BY2',    2);
-define('BY21',   3);
-define('BY25',   4);
-define('BY3',    5);
-define('BYSA1',  6);
-define('BYSA2',  7);
-define('BYSA21', 8);
-define('BYSA25', 9);
-define('BYSA3', 10);
-define('LAL',   11);
+define('BY1',     1);
+define('BY2',     2);
+define('BY21',    3);
+define('BY25',    4);
+define('BY3',     5);
+define('BYSA1',   6);
+define('BYSA2',   7);
+define('BYSA21',  8);
+define('BYSA25',  9);
+define('BYSA3',  10);
+define('LAL',    11);
 
 // Arrays containing regular expressions for each license type
 // (so we can support multiple URL formats in the future if needed)
