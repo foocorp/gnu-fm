48ba50a9e28d06079c9e4a1141db18d1bac76ee8
Mike Sheldon <elleo@gnu.org>
2011-09-22 22:21:57 +0100

    Make it possible to switch to a mobile theme via a get parameter

--- nixtape/templating.php
+++ nixtape/templating.php
@@ -45,20 +45,26 @@ if (isset($_GET['lang'])) {
 }
 $current_lang = setlocale(LC_ALL, $languages);
 
-bindtextdomain('nixtape', $install_path . '/themes/' . $default_theme . '/locale/');
+if(isset($_GET['mobile']) && $_GET['mobile']) {
+	$theme = 'mobile';
+} else {
+	$theme = $default_theme;
+}
+
+bindtextdomain('nixtape', $install_path . '/themes/' . $theme . '/locale/');
 textdomain('nixtape');
 
 $smarty = new Smarty();
 
-$smarty->template_dir = $install_path . '/themes/'. $default_theme . '/templates/';
-$smarty->compile_dir = $install_path. '/themes/' . $default_theme . '/templates_c/';
+$smarty->template_dir = $install_path . '/themes/'. $theme . '/templates/';
+$smarty->compile_dir = $install_path. '/themes/' . $theme . '/templates_c/';
 $smarty->cache_dir = $install_path. '/cache/';
 
 $current_lang = preg_replace('/.UTF-8/', '', $current_lang);
 $smarty->assign('lang_selector_array', array(($current_lang) => 1));
 $smarty->assign('base_url', $base_url);
 $smarty->assign('default_theme', $default_theme);
-$smarty->assign('img_url', $base_url . '/themes/' . $default_theme . '/img/');
+$smarty->assign('img_url', $base_url . '/themes/' . $theme . '/img/');
 $smarty->assign('this_page', $_SERVER['REQUEST_URI']);
 $smarty->assign('this_page_absolute',
 	  (empty($_SERVER['HTTPS']) ? 'http://' : 'http://')
