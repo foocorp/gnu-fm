d224510509562ac3383608afe46e9884b01e2879
Mike Sheldon <elleo@gnu.org>
2011-10-02 20:24:25 +0100

    Display correct error when checking tags for a new track

--- nixtape/track-add.php
+++ nixtape/track-add.php
@@ -107,7 +107,7 @@ if (isset($_POST['submit'])) {
 					}
 				} elseif (preg_match('/TITLE=(.*)/i', $comment, $comment_matches)) {
 					if (strtolower($comment_matches[1]) != strtolower($name)) {
-						$errors[] = 'The track name tag in the uploaded file doesn\'t match the name of the track you\'re editing. The track name tag should be: "' . $track->name . '", but currently it\'s: "' . $comment_matches[1] . '".';
+						$errors[] = 'The track name tag in the uploaded file doesn\'t match the name of the track you\'re editing. The track name tag should be: "' . $name . '", but currently it\'s: "' . $comment_matches[1] . '".';
 						$tag_errors = true;
 					}
 				}
