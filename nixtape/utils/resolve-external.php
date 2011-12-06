0e4404193f1c5bf19a3316b8ff30c1f410ed9fa5
Mike Sheldon <elleo@gnu.org>
2011-12-06 19:54:43 +0000

    Restore jamendo links until more efficient mirroring can be handled

--- nixtape/utils/resolve-external.php
+++ nixtape/utils/resolve-external.php
@@ -10,7 +10,7 @@ function resolve_external_url($url) {
 function process_jamendo_url($url) {
 	if (substr($url, 10, 13) == 'track/stream/') {
 		$id = substr($url, 23);
-		return 'http://gigue.rrbone.net/' . $id . '.ogg2';
+		return 'http://api.jamendo.com/get2/stream/track/redirect/?id=' . $id . '&streamencoding=ogg2';
 	}
 
 	if (substr($url, 10, 15) == 'album/download/') {
