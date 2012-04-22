952e1384153a9516aa01144a1dc484a7157cbb68
Matt Lee <mattl@cnuk.org>
2012-04-22 14:31:07 -0400

    added very basic caching headers

--- nixtape/auth.php
+++ nixtape/auth.php
@@ -50,4 +50,10 @@ if (isset($_COOKIE['session_id'])) {
 			$err = 1;
 		}
 	}
+} else {
+$seconds_to_cache = 3600;
+$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+header("Expires: $ts");
+header("Pragma: cache");
+header("Cache-Control: max-age=$seconds_to_cache");
 }
