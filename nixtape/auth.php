e48af5b9b2fc444556c2c5275719add633faa03d
Mike Sheldon <elleo@gnu.org>
2012-04-23 18:25:02 +0100

    Remove language cookie for logged out users

--- nixtape/auth.php
+++ nixtape/auth.php
@@ -55,4 +55,5 @@ $seconds_to_cache = 3600;
 $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 header("Expires: $ts");
 header("Cache-Control: max-age=$seconds_to_cache");
+setcookie('lang', '', time() - 3600);
 }
