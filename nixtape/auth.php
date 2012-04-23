b13c8ab59a28a0d6acce3cce53d5fd6a793550a4
Mike Sheldon <elleo@gnu.org>
2012-04-23 18:21:28 +0100

    Remove pragma header

--- nixtape/auth.php
+++ nixtape/auth.php
@@ -54,6 +54,5 @@ if (isset($_COOKIE['session_id'])) {
 $seconds_to_cache = 3600;
 $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 header("Expires: $ts");
-header("Pragma: cache");
 header("Cache-Control: max-age=$seconds_to_cache");
 }
