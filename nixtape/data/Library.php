2bbef339c25eabb042c55ddbbd47c9f9e7df0e0e
Jonas Haraldsson <jonas227@gmail.com>
2013-02-24 22:20:00 +0100

    year in license

--- nixtape/data/Library.php
+++ nixtape/data/Library.php
@@ -2,7 +2,7 @@
 
 /* GNU FM -- a free network service for sharing your music listening habits
 
-   Copyright (C) 2009 Free Software Foundation, Inc
+   Copyright (C) 2013 Free Software Foundation, Inc
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Affero General Public License as published by
